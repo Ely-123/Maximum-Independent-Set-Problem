0abdde686ea2edad47c60d438b84d58ca35c8442
Jocsan Ely <jocsanelyrueda@gmail.com>
2024-04-19 22:51:49 -0600

    Clase grafo

---
+++ Grafo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>;
+#include "vertice.h";
+#include <vector>;
+using namespace std;
+
+class Grafo
+{
+private:
+    vector<Vertice> *matriz;
+
+public:
+    Grafo(/* args */);
+    ~Grafo();
+};
+
+Grafo::Grafo(/* args */)
+{
+}
+
+Grafo::~Grafo()
+{
+}
