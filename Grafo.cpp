fbd962b5f14f7d855323c0fdf18f965afe6a62e1
Jocsan Ely <jocsanelyrueda@gmail.com>
2024-04-24 08:14:37 -0600

    Documentacion

--- Grafo.cpp
+++ Grafo.cpp
@@ -3,6 +3,17 @@
 #include "grafo.h"
 using namespace std;
 
+/**
+ * @brief Constructor de la clase Grafo que crea un grafo a partir de un conjunto de aristas.
+ *
+ * @param aristas Conjunto de aristas representado como un conjunto de arrays de enteros de tamaño 2, donde cada array representa una arista entre dos vértices.
+ *
+ * Este constructor toma un conjunto de aristas y construye un grafo no dirigido a partir de ellas.
+ *
+ * @note Se asume que la clase Grafo tiene un miembro de datos E de tipo puntero a un mapa de enteros a vectores de enteros.
+ *       Este mapa representa la estructura del grafo donde las claves son los vértices y los valores son vectores que contienen los vecinos de cada vértice.
+ *       Además, se asume que la clase Conjunto<T> tiene métodos como getCard() para obtener el número de elementos, at() para acceder a un elemento por índice y count() para verificar la existencia de un elemento.
+ */
 Grafo::Grafo(Conjunto<array<int, 2>> aristas)
 
 {
@@ -23,7 +34,16 @@ Grafo::Grafo(Conjunto<array<int, 2>> aristas)
         (*E)[v2].push_back(v1);
     }
 }
-
+/**
+ * @brief Constructor de la clase Grafo que crea un grafo a partir de un mapa de vértices y sus vecinos.
+ *
+ * @param grafo Mapa que representa el grafo, donde las claves son los vértices y los valores son vectores que contienen los vecinos de cada vértice.
+ *
+ * Este constructor toma un mapa que representa la estructura del grafo, donde las claves son los vértices y los valores son vectores que contienen los vecinos de cada vértice, y construye un grafo no dirigido a partir de esta estructura.
+ *
+ * @note Se asume que la clase Grafo tiene un miembro de datos E de tipo puntero a un mapa de enteros a vectores de enteros.
+ *       Este mapa representa la estructura del grafo donde las claves son los vértices y los valores son vectores que contienen los vecinos de cada vértice.
+ */
 Grafo::Grafo(map<int, vector<int>> grafo)
 {
     E = new map<int, vector<int>>;
@@ -45,6 +65,14 @@ int Grafo::getSize()
     return (*E).size();
 }
 
+/**
+ * @brief Método de la clase Grafo que genera un archivo de imagen representando el grafo utilizando Graphviz.
+ * 
+ * Este método genera un archivo de imagen que representa visualmente el grafo utilizando el software Graphviz. El grafo se representa como un gráfico no dirigido donde los vértices están conectados por aristas.
+ * 
+ * @note Se asume que la clase Grafo tiene un miembro de datos E de tipo puntero a un mapa de enteros a vectores de enteros, que representa la estructura del grafo donde las claves son los vértices y los valores son vectores que contienen los vecinos de cada vértice.
+ *       Además, se asume que la función getSize() devuelve el número de vértices en el grafo.
+ */
 void Grafo::dibujar()
 {
     bool *visitados = new bool[getSize()];
